a9c45758d9fbf5da7b82ea2dd9f2e07b729153ca
ArtMaestro <Sivak2097@gmail.com>
2024-02-22 01:28:18 +0300

    fixed bug with zeros

--- sum.go
+++ sum.go
@@ -2,27 +2,27 @@ package main
 
 import "fmt"
 
-func maestro(nums []int, target int) (int, int) {
+func maestro(nums []int, target int) (int, int, bool) {
 	nMap := make(map[int]int)
 
 	for i, num := range nums {
 		comp := target - num
 		if j, ok := nMap[comp]; ok {
-			return nums[j], num
+			return nums[j], num, true
 		}
 		nMap[num] = i
 	}
 
-	return 0, 0
+	return 0, 0, false
 }
 
 func main() {
 	nums := []int{8, 1, 14, 22}
-	target := 9
+	target := 0
 
-	num1, num2 := maestro(nums, target)
+	num1, num2, ok := maestro(nums, target)
 
-	if num1 != 0 && num2 != 0 {
+	if ok {
 		fmt.Printf("Два числа из массива %v, сумма которых равна %d: %d и %d\n", nums, target, num1, num2)
 	} else {
 		fmt.Printf("В массиве %v нет двух чисел, сумма которых равна %d\n", nums, target)
